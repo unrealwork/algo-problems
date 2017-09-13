59d41cdb3c1afb12e1e451de3ab423f9f435ebe7
Igor Shmagrinskiy <ishamgrinskiy@marathonbet.ru>
2017-09-13 13:59:34 +0300

    hackerrank: fp: introducion: evaluating e^x: fix test

--- src/test/scala/com/hackerrank/fp/introduction/evalex/EvalExSuite.scala
+++ src/test/scala/com/hackerrank/fp/introduction/evalex/EvalExSuite.scala
@@ -5,9 +5,10 @@ import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class EvalExSuite extends FunSuite{
+class EvalExSuite extends FunSuite {
   val Precision = 1e-1
   test("Description e^20") {
-    assert((EvalEx.exp(20) - 2423600.1887d).abs < Precision)
+    lazy val expectedValue = 2423600.1887d
+    assert((EvalEx.exp(20) - expectedValue).abs < Precision)
   }
 }
