2825d21922df24acdedff0fccdf77e04980f1b13
unrealwork <unrealwork@gmail.com>
2017-11-30 15:22:48 +0300

    hackerrank: fp: recursion: string-o-permute

---
+++ src/test/scala/com/hackerrank/fp/recursion/StringPermute.scala
@@ -0,0 +1,23 @@
+package com.hackerrank.fp.recursion
+
+import scala.io.StdIn
+
+object StringPermute {
+  private def solve(s: String): String = {
+    def listRes(list: List[Char]): List[Char] = {
+      list match {
+        case Nil => List()
+        case left :: right :: rest => right :: left :: listRes(rest)
+      }
+    }
+
+    listRes(s.toList).mkString
+  }
+
+  def main(args: Array[String]): Unit = {
+    val casesCount = StdIn.readInt()
+    Stream.range(0, casesCount)
+      .map(_ => solve(StdIn.readLine()))
+      .foreach(println)
+  }
+}
