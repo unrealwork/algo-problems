36a01e2fe975f68a48f11278538178482a05a669
unrealwork <unrealwork@gmail.com>
2018-02-08 11:27:25 +0500

    hackerrank: algo: implementation: taum and bday(unsolved)

---
+++ src/main/java/com/hackerrank/algorithms/implementation/TaumAndBday.java
@@ -0,0 +1,37 @@
+package com.hackerrank.algorithms.implementation;
+
+import java.util.Scanner;
+
+public class TaumAndBday {
+    static long taumBday(int b, int w, int x, int y, int z) {
+        // Complete this function
+        return giftCost(bestBlackCost(x, y, z), bestWhiteCost(x, y, z), b, w);
+    }
+
+    static int bestBlackCost(int x, int y, int z) {
+        return Math.min(x, z + y);
+    }
+
+    static int bestWhiteCost(int x, int y, int z) {
+        return Math.min(y, x + z);
+    }
+
+    static long giftCost(int bestBlack, int bestWhite, int b, int w) {
+        return b * bestBlack + bestWhite * w;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int a0 = 0; a0 < t; a0++) {
+            int b = in.nextInt();
+            int w = in.nextInt();
+            int x = in.nextInt();
+            int y = in.nextInt();
+            int z = in.nextInt();
+            long result = taumBday(b, w, x, y, z);
+            System.out.println(result);
+        }
+        in.close();
+    }
+}
