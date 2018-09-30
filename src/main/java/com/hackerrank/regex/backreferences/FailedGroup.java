5ac8b3cc283fa5675c7c136270d1143cef5e262f
unrealwork <unrealwork@gmail.com>
2018-09-30 05:48:22 +0300

    failed group usage

--- src/main/java/com/hackerrank/regex/backreferences/FailedGroup.java
+++ src/main/java/com/hackerrank/regex/backreferences/FailedGroup.java
@@ -6,6 +6,6 @@ public class FailedGroup {
 
     public static void main(String[] args) {
         Regex_Test tester = new Regex_Test();
-        tester.checker("^\\d\\d(-?)?(\\d\\d\\1){2}\\d\\d$"); // Use \\ instead of using \
+        tester.checker("^\\d{2}(-?)?(\\d{2}\\1){2}\\d{2}$"); // Use \\ instead of using \
     }
 }
