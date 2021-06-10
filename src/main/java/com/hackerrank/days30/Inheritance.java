86eaef89f752dec7ee147dcab76f38628bac64f5
unrealwork <unrealwork@gmail.com>
2021-06-10 20:57:43 +0500

    hackerrank: 30days: inheritance

---
+++ src/main/java/com/hackerrank/days30/Inheritance.java
@@ -0,0 +1,96 @@
+package com.hackerrank.days30;
+
+
+import java.util.*;
+
+class Person {
+    protected String firstName;
+    protected String lastName;
+    protected int idNumber;
+
+    // Constructor
+    Person(String firstName, String lastName, int identification) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = identification;
+    }
+
+    // Print person data
+    public void printPerson() {
+        System.out.println(
+                "Name: " + lastName + ", " + firstName
+                        + "\nID: " + idNumber);
+    }
+
+}
+
+class Student extends Person {
+    private int[] testScores;
+
+    Student(String firstName, String lastName, int identification, int[] scores) {
+        super(firstName, lastName, identification);
+        this.testScores = Arrays.copyOf(scores, scores.length);
+    }
+
+    public String calculate() {
+        final double avg = Arrays.stream(testScores).sum() / testScores.length;
+        if (avg < 40) {
+            return "T";
+        }
+        if (avg >= 90 && avg <= 100) {
+            return "O";
+        }
+        if (avg >= 70 && avg < 80) {
+            return "A";
+        }
+
+        if (avg >= 80 && avg < 90) {
+            return "E";
+        }
+
+        if (avg >= 55 && avg < 70) {
+            return "P";
+        }
+
+        if (avg >= 40 && avg < 55) {
+            return "D";
+        }
+        throw new IllegalStateException("Invalid avg score");
+    }
+
+    /*
+     *   Class Constructor
+     *
+     *   @param firstName - A string denoting the Person's first name.
+     *   @param lastName - A string denoting the Person's last name.
+     *   @param id - An integer denoting the Person's ID number.
+     *   @param scores - An array of integers denoting the Person's test scores.
+     */
+    // Write your constructor here
+
+    /*
+     *   Method Name: calculate
+     *   @return A character denoting the grade.
+     */
+    // Write your method here
+
+}
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String firstName = scan.next();
+        String lastName = scan.next();
+        int id = scan.nextInt();
+        int numScores = scan.nextInt();
+        int[] testScores = new int[numScores];
+        for (int i = 0; i < numScores; i++) {
+            testScores[i] = scan.nextInt();
+        }
+        scan.close();
+
+        Student s = new Student(firstName, lastName, id, testScores);
+        s.printPerson();
+        System.out.println("Grade: " + s.calculate());
+    }
+}
