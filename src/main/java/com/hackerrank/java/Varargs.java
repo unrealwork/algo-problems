8bc598d9776e7d7624159057ed2aa750d6f23360
unrealwork <unrealwork@gmail.com>
2021-06-11 17:12:59 +0500

    hackerrank: java: Varargs

---
+++ src/main/java/com/hackerrank/java/Varargs.java
@@ -0,0 +1,56 @@
+package com.hackerrank.java;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+
+class Add {
+    public void add(int... nums) {
+        final StringBuilder res = new StringBuilder(Integer.toString(nums[0]));
+        long sum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum+=nums[i];
+            res.append(String.format("+%d", nums[i]));
+        }
+        res.append(String.format("=%d",sum));
+        System.out.println(res);
+    }
+}
+
+public class Varargs {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n1 = Integer.parseInt(br.readLine());
+            int n2 = Integer.parseInt(br.readLine());
+            int n3 = Integer.parseInt(br.readLine());
+            int n4 = Integer.parseInt(br.readLine());
+            int n5 = Integer.parseInt(br.readLine());
+            int n6 = Integer.parseInt(br.readLine());
+            Add ob = new Add();
+            ob.add(n1, n2);
+            ob.add(n1, n2, n3);
+            ob.add(n1, n2, n3, n4, n5);
+            ob.add(n1, n2, n3, n4, n5, n6);
+            Method[] methods = Add.class.getDeclaredMethods();
+            Set<String> set = new HashSet<>();
+            boolean overload = false;
+            for (int i = 0; i < methods.length; i++) {
+                if (set.contains(methods[i].getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(methods[i].getName());
+
+            }
+            if (overload) {
+                throw new Exception("Overloading not allowed");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
