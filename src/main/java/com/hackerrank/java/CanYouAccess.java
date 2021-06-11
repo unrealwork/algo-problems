2bf3775804d61945a790f8a7663d46c4acae615d
unrealwork <unrealwork@gmail.com>
2021-06-11 17:40:59 +0500

    hackerrank: java: CanYouAccess

---
+++ src/main/java/com/hackerrank/java/CanYouAccess.java
@@ -0,0 +1,63 @@
+package com.hackerrank.java;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.security.Permission;
+
+
+public class CanYouAccess {
+
+    public static void main(String[] args) throws Exception {
+        DoNotTerminate.forbidExit();
+
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int num = Integer.parseInt(br.readLine().trim());
+            Object o;
+            final Class<?> privateClass = Inner.class.getDeclaredClasses()[0];
+            final Constructor constructor = privateClass.getDeclaredConstructors()[0];
+            constructor.setAccessible(true);
+            o = constructor.newInstance(new Inner());// Must be used to hold the reference of the instance of the class Solution.Inner.Private
+            final Class<?> aClass = o.getClass();
+            Method method = aClass.getDeclaredMethods()[0];
+            method.setAccessible(true);
+            System.out.printf("%d is %s%n", num,method.invoke(o, num));
+            System.out.println("An instance of class: " + o.getClass().getCanonicalName() + " has been created");
+        }//end of try
+
+        catch (DoNotTerminate.ExitTrappedException e) {
+            System.out.println("Unsuccessful Termination!!");
+        }
+    }//end of main
+
+    static class Inner {
+        private class Private {
+            private String powerof2(int num) {
+                return ((num & num - 1) == 0) ? "power of 2" : "not a power of 2";
+            }
+        }
+    }//end of Inner
+
+}//end of Solution
+
+class DoNotTerminate { //This class prevents exit(0)
+
+    public static class ExitTrappedException extends SecurityException {
+
+        private static final long serialVersionUID = 1L;
+    }
+
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
+}
