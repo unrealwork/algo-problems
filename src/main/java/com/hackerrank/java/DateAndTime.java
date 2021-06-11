e65ed38d7a3f33c3c1bb3ab68d8a9de35a65ed63
unrealwork <unrealwork@gmail.com>
2021-06-11 16:04:26 +0500

    hackerrank: java: DateAndTime

---
+++ src/main/java/com/hackerrank/java/DateAndTime.java
@@ -0,0 +1,49 @@
+package com.hackerrank.java;
+
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        return LocalDate.of(year, month, day).getDayOfWeek()
+                .getDisplayName(TextStyle.FULL, Locale.ENGLISH)
+                .toUpperCase(Locale.ENGLISH);
+    }
+
+}
+
+public class DateAndTime {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
