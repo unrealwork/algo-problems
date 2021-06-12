f4adc86d6fb13ce4b07379a1bd2f18dbe932c202
unrealwork <unrealwork@gmail.com>
2021-06-12 18:07:24 +0500

    hackerrank: basic: PatternSyntax

---
+++ src/main/java/com/hackerrank/java/strings/PatternSyntax.java
@@ -0,0 +1,23 @@
+package com.hackerrank.java.strings;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class PatternSyntax {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0){
+            String pattern = in.nextLine();
+            try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch (PatternSyntaxException e) {
+                System.out.println("Invalid");
+            }
+            testCases--;
+            //Write your code
+        }
+    }
+}
