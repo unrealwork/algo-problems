ec259451ec717d1c70c42b0bbcebafb6ec4ec941
unrealwork <unrealwork@gmail.com>
2021-06-12 16:29:37 +0500

    hackerrank: basic: Substring

---
+++ src/main/java/com/hackerrank/java/strings/Substring.java
@@ -0,0 +1,13 @@
+package com.hackerrank.java.strings;
+
+import java.util.Scanner;
+
+public class Substring {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start, end));
+    }
+}
