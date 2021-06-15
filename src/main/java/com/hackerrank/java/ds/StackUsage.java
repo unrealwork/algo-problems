e34c3a6e3ec9b1515e01631934cdbbc03c80d697
unrealwork <unrealwork@gmail.com>
2021-06-15 18:52:23 +0500

    hackerrank: java: StackUsage

---
+++ src/main/java/com/hackerrank/java/ds/StackUsage.java
@@ -0,0 +1,77 @@
+package com.hackerrank.java.ds;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class StackUsage {
+
+
+    public static void main(String[] argh) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String input = sc.next();
+            //Complete the code
+            System.out.println(isValid(input));
+        }
+    }
+
+    private static boolean isValid(final String s) {
+        final Stack<Parenthesis> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            final char c = s.charAt(i);
+            final Parenthesis curP = Parenthesis.from(c);
+            if (stack.isEmpty()) {
+                if (curP.isOpen(c)) {
+                    stack.push(curP);
+                } else {
+                    return false;
+                }
+            } else {
+                final Parenthesis lastP = stack.peek();
+                if (lastP == curP) {
+                    if (!curP.isOpen(c)) {
+                        stack.pop();
+                    } else {
+                        stack.push(curP);
+                    }
+                } else {
+                    if (curP.isOpen(c)) {
+                        stack.push(curP);
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    private static enum Parenthesis {
+        CURLY('{', '}'), SQUARE('[', ']'), SIMPLE('(', ')');
+        private final char openSymbol;
+        private final char closeSymbol;
+
+        Parenthesis(final char open, final char close) {
+            openSymbol = open;
+            closeSymbol = close;
+        }
+
+        public boolean isOpen(char c) {
+            return c == openSymbol;
+        }
+
+        public static Parenthesis from(char c) {
+            for (Parenthesis p : Parenthesis.values()) {
+                if (p.closeSymbol == c || p.openSymbol == c) {
+                    return p;
+                }
+            }
+            throw new IllegalStateException();
+        }
+    }
+}
+
+
+
+
