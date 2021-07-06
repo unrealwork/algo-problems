955fda38f94bf6281a0941007925bcbc3936417b
unrealwork <unrealwork@gmail.com>
2021-07-06 21:37:26 +0300

    hackerrank: prep: stack: Encryption

---
+++ src/main/java/com/hackerrank/algorithms/strings/Encryption.java
@@ -0,0 +1,63 @@
+package com.hackerrank.algorithms.strings;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+
+public class Encryption {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.encryption(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    private static class Result {
+
+        /*
+         * Complete the 'encryption' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts STRING s as parameter.
+         */
+
+        public static String encryption(String s) {
+            // Write your code here
+            final String noWhitespaces = s.replace("\\w+", "");
+            final double sqrt = Math.sqrt(noWhitespaces.length());
+            final int columns = (int) Math.ceil(sqrt);
+            final int rows = (int) Math.floor(sqrt);
+            final int size = columns + noWhitespaces.length();
+            int j = 0;
+            int i = 0;
+            final char[] symbols = new char[size];
+            Arrays.fill(symbols, ' ');
+            for (int k = 0; k < noWhitespaces.length(); k++) {
+                final char c = noWhitespaces.charAt(k);
+                symbols[j * rows + i + j] = c;
+                j++;
+                if (j == columns) {
+                    j = 0;
+                    i++;
+                }
+            }
+            return new String(symbols).trim().replaceAll(" +", " ");
+        }
+
+    }
+}
+
