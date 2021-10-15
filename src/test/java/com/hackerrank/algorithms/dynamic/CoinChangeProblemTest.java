da455e27f43db8a244c6f5361ba4131a30bae3e0
unrealwork <unrealwork@gmail.com>
2021-10-15 13:59:29 +0300

    coin change problem

---
+++ src/test/java/com/hackerrank/algorithms/dynamic/CoinChangeProblemTest.java
@@ -0,0 +1,12 @@
+package com.hackerrank.algorithms.dynamic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CoinChangeProblemTest {
+
+    @Test
+    public void getWays() {
+    }
+}
