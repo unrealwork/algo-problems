24acc93abc4386050177ffea89553f469abcf8ca
unrealwork <unrealwork@gmail.com>
2021-10-15 14:01:01 +0300

    coin change problem

--- src/main/java/com/hackerrank/algorithms/dynamic/CoinChangeProblem.java
+++ src/main/java/com/hackerrank/algorithms/dynamic/CoinChangeProblem.java
@@ -1,2 +1,37 @@
-package com.hackerrank.algorithms.dynamic;public class CoinChangeProblem {
+package com.hackerrank.algorithms.dynamic;
+
+
+import java.util.List;
+
+
+public class CoinChangeProblem {
+    public static long getWays(int n, List<Long> c) {
+        long[][] res = new long[c.size()][n + 1];
+        int[] coins = c.stream().mapToInt(Long::intValue)
+                .sorted().toArray();
+        for (int i = 0; i < coins.length; i++) {
+            int coin = coins[i];
+            if (coin <= n) {
+                res[i][coin] = 1;
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int k = 0; k < coins.length; k++) {
+                int co = coins[k];
+                for (int j = 0; j <= k; j++) {
+                    if (res[j][i] > 0) {
+                        if (co + i <= n) {
+                            res[k][co + i] += res[j][i];
+                        }
+                    }
+                }
+            }
+        }
+        long sum = 0;
+        for (int i = 0; i < coins.length; i++) {
+            sum += res[i][n];
+        }
+        return sum;
+    }
 }
--- src/test/java/com/hackerrank/algorithms/dynamic/CoinChangeProblemTest.java
+++ src/test/java/com/hackerrank/algorithms/dynamic/CoinChangeProblemTest.java
@@ -1,12 +1,58 @@
 package com.hackerrank.algorithms.dynamic;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
+import static java.util.Arrays.asList;
+
+@RunWith(Parameterized.class)
 public class CoinChangeProblemTest {
+    public final TestCase testCase;
+
+    public CoinChangeProblemTest(TestCase testCase) {
+        this.testCase = testCase;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> testCases() {
+        return asList(new Object[][]{
+                {TestCase.testCase(
+                        4, 4, 1, 2, 3)},
+
+                {TestCase.testCase(
+                        5, 10, 2, 5, 3, 6)}
+        });
+    }
 
     @Test
-    public void getWays() {
+    public void testGetWays1() {
+        final long actual = CoinChangeProblem.getWays(testCase.n, testCase.coins);
+        Assert.assertEquals(testCase.expected, actual);
+    }
+
+    private static class TestCase {
+        private final List<Long> coins;
+        private final long expected;
+        private final int n;
+
+        private TestCase(long expected, int n, List<Long> coins) {
+            this.n = n;
+            this.coins = coins;
+            this.expected = expected;
+        }
+
+        private static TestCase testCase(long expected, int n, int... coins) {
+            List<Long> list = new ArrayList<>(coins.length);
+            for (int coin : coins) {
+                list.add((long) coin);
+            }
+            return new TestCase(expected, n, list);
+        }
     }
 }
