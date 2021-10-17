8ef635accc92d20b1968bd3403dc2c52b25c59a7
unrealwork <unrealwork@gmail.com>
2021-10-17 08:57:41 +0300

    MinimumSwaps: passed

--- src/main/java/com/hackerrank/algorithms/dynamic/MinimumSwaps.java
+++ src/main/java/com/hackerrank/algorithms/dynamic/MinimumSwaps.java
@@ -1,24 +1,22 @@
 package com.hackerrank.algorithms.dynamic;
 
 public class MinimumSwaps {
-    private MinimumSwaps() {
-        
-    }
+  private MinimumSwaps() {}
 
-    static int minimumSwaps(int[] arr) {
-        int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            while (arr[i] != (i + 1)) {
-                swap(i, arr[i] - 1, arr);
-                count++;
-            }
-        }
-        return count;
+  static int minimumSwaps(int[] arr) {
+    int count = 0;
+    for (int i = 0; i < arr.length; i++) {
+      while (arr[i] != (i + 1)) {
+        swap(i, arr[i] - 1, arr);
+        count++;
+      }
     }
+    return count;
+  }
 
-    private static void swap(int i, int j, int[] arr) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
-    }
+  private static void swap(int i, int j, int[] arr) {
+    int tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+  }
 }
--- src/test/java/com/hackerrank/algorithms/dynamic/MinimumSwapsTest.java
+++ src/test/java/com/hackerrank/algorithms/dynamic/MinimumSwapsTest.java
@@ -12,41 +12,38 @@ import static java.util.Arrays.asList;
 
 @RunWith(Parameterized.class)
 public class MinimumSwapsTest {
-    private final TestCase testCase;
+  private final TestCase testCase;
 
-    public MinimumSwapsTest(TestCase testCase) {
-        this.testCase = testCase;
-    }
+  public MinimumSwapsTest(TestCase testCase) {
+    this.testCase = testCase;
+  }
 
-    @Parameterized.Parameters
-    public static Collection<Object[]> testCases() {
-        return asList(new Object[][]{
-                {testCase(3,
-                        4, 3, 1, 2)},
-                {testCase(3,
-                        2, 3, 4, 1, 5)
-                },
-                {testCase(3,
-                        1, 3, 5, 2, 4, 6, 7)}
+  @Parameterized.Parameters
+  public static Collection<Object[]> testCases() {
+    return asList(
+        new Object[][] {
+          {testCase(3, 4, 3, 1, 2)},
+          {testCase(3, 2, 3, 4, 1, 5)},
+          {testCase(3, 1, 3, 5, 2, 4, 6, 7)}
         });
+  }
+
+  @Test
+  public void minimumSwaps() {
+    Assert.assertEquals(testCase.expected, MinimumSwaps.minimumSwaps(testCase.arr));
+  }
+
+  static class TestCase {
+    private final int expected;
+    private final int[] arr;
+
+    private TestCase(int expected, int[] arr) {
+      this.expected = expected;
+      this.arr = arr;
     }
 
-    @Test
-    public void minimumSwaps() {
-        Assert.assertEquals(testCase.expected, MinimumSwaps.minimumSwaps(testCase.arr));
+    static TestCase testCase(int expected, int... arr) {
+      return new TestCase(expected, arr);
     }
-
-    static class TestCase {
-        private final int expected;
-        private final int[] arr;
-
-        private TestCase(int expected, int[] arr) {
-            this.expected = expected;
-            this.arr = arr;
-        }
-
-        static TestCase testCase(int expected, int... arr) {
-            return new TestCase(expected, arr);
-        }
-    }
+  }
 }
