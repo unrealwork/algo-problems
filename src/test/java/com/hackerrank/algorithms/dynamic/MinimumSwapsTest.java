53d98f1cf82e6b32027adc4a353fb980301e2fb9
unrealwork <unrealwork@gmail.com>
2021-10-17 08:55:09 +0300

    MinimumSwaps: passed

---
+++ src/test/java/com/hackerrank/algorithms/dynamic/MinimumSwapsTest.java
@@ -0,0 +1,51 @@
+package com.hackerrank.algorithms.dynamic;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
+
+import static com.hackerrank.algorithms.dynamic.MinimumSwapsTest.TestCase.testCase;
+import static java.util.Arrays.asList;
+
+@RunWith(Parameterized.class)
+public class MinimumSwapsTest {
+    private final TestCase testCase;
+
+    public MinimumSwapsTest(TestCase testCase) {
+        this.testCase = testCase;
+    }
+    @Parameterized.Parameters
+    public static final Collection<Object[]> testCases() {
+        return asList(new Object[][]{
+                {testCase(3, 
+                        4, 3, 1, 2)},
+                {testCase(3,
+                        2, 3, 4, 1, 5)
+                },
+                {testCase(3,
+                        1, 3, 5, 2, 4, 6, 7)}
+        });
+    }
+
+    @Test
+    public void minimumSwaps() {
+        Assert.assertEquals(testCase.expected, MinimumSwaps.minimumSwaps(testCase.arr));
+    }
+
+    static class TestCase {
+        private final int expected;
+        private final int[] arr;
+
+        private TestCase(int expected, int[] arr) {
+            this.expected = expected;
+            this.arr = arr;
+        }
+
+        static TestCase testCase(int expected, int... arr) {
+            return new TestCase(expected, arr);
+        }
+    }
+}
