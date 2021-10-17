9fd6ea498dd56621788977c60b64dbed41278190
unrealwork <unrealwork@gmail.com>
2021-10-17 09:01:08 +0300

    MinimumSwaps: javadoc

--- src/main/java/com/hackerrank/algorithms/dynamic/MinimumSwaps.java
+++ src/main/java/com/hackerrank/algorithms/dynamic/MinimumSwaps.java
@@ -1,5 +1,12 @@
 package com.hackerrank.algorithms.dynamic;
 
+/**
+ * You are given an unordered array consisting of consecutive integers [1, 2, 3, ..., n] without any
+ * duplicates. You are allowed to swap any two elements. Find the minimum number of swaps required
+ * to sort the array in ascending order.
+ *
+ * @see <a href="https://www.hackerrank.com/challenges/minimum-swaps-2/problem">Minimum Swaps 2</a>
+ */
 public class MinimumSwaps {
   private MinimumSwaps() {}
 
