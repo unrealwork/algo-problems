ed606409f91b324b3e9e726c2986b2ed2b0db396
Igor Shmagrinskiy <unrealwork@gmail.com>
2021-11-08 10:55:18 +0300

    leetcode: dp: UniquePaths: passed

---
+++ src/main/java/com/leetcode/dp/UniquePaths.java
@@ -0,0 +1,20 @@
+package com.leetcode.dp;
+
+public class UniquePaths {
+    private UniquePaths() {
+        
+    }
+
+    public static int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != 0 || j != 0) {
+                    dp[i][j] = (i > 0 ? dp[i - 1][j] : 0) + (j > 0 ? dp[i][j - 1] : 0);
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
---
+++ src/test/java/com/leetcode/dp/UniquePathsTest.java
@@ -0,0 +1,10 @@
+package com.leetcode.dp;
+
+import junit.framework.TestCase;
+
+public class UniquePathsTest extends TestCase {
+
+    public void testUniquePaths() {
+        assertEquals(28, UniquePaths.uniquePaths(3, 7));
+    }
+}
