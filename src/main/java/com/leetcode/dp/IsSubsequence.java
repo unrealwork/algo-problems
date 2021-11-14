27fa6b8cc61f5fd988ab4ec6258f0a25922f86bd
Igor Shmagrinskiy <unrealwork@gmail.com>
2021-11-15 00:17:59 +0300

    leetcode: dp: IsSubsequence: passed

---
+++ src/main/java/com/leetcode/dp/IsSubsequence.java
@@ -0,0 +1,18 @@
+package com.leetcode.dp;
+
+public class IsSubsequence {
+    private IsSubsequence() {
+    }
+
+    public static boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
+    }
+}
